49c4de5b18dac18e519ec3f5cb95d38479690b88
Morten Punnerud Engelstad <morten@punnerud.net>
2022-05-21 09:57:31 +0200

    Added admin panel for todos

--- lists/admin.py
+++ lists/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from lists.models import Todo
+
+class TodoAdmin(admin.ModelAdmin):
+	list_display = ('creator','description','created_at','finished_at','is_finished')
+
+admin.site.register(Todo, TodoAdmin)
+
+
+# Register your models here.
+from lists.models import TodoList
+
+class TodolistAdmin(admin.ModelAdmin):
+	list_display = ('creator','title','created_at','count','count_finished','count_open')
+
+admin.site.register(TodoList, TodolistAdmin)
